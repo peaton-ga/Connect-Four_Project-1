831f990a974e7beed5911701fe17d591e7bf0261
peaton373 <peaton373@gmail.com>
2020-03-17 15:05:25 -0500

    pseudo coded JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log("Connect-Four")
+// console.log("Connect-Four")
 
 // Declare global variables
 const td = document.querySelectorAll('td');
@@ -16,15 +16,6 @@ for(let i = 0; i < td.length; i++) {
 	// Listener responsible for mouseover pullOut
 	td[i].addEventListener('mouseover', pullOutIntro);
 }
-
-
-
-
-
-
-
-
-
 
 // Declare selectColumn function
 function selectColumn() {
@@ -44,5 +35,60 @@ function selectColumn() {
 		}
 	}
 }
+// Declare function to display chip over columns //
 
 
+
+// Declare function that will drop the chip over the columns //
+	// Use global variables for functions
+		// Create a new chip to drop
+			// Create inner circle for effect showing shadow on chip (figure out way to animate)
+				// Create a variable that can hold the amount of open spots within column
+				 	// Put the chip together (use append Child?)
+
+
+// Declare function that will change the chip
+	// If chip isn't yellow then chip is now yellow
+		// Else it should be diff color, (red?)
+
+
+
+
+
+// Declare place chip function? //
+	// Grab selected column
+		// Array for holding each td with selected column
+
+// Call the place chip function? pass to current td //
+// Call function to change chip --> Two players
+
+// Declare a function to check the board //
+	// thought about making array variable to store every possible win combo? (not yet, functionality first and foremost!)
+
+
+// Use loop through all td's created
+	// If td has a column, add td to array (create a filter array?)
+	// The filter() method creates an array filled with all array elements 
+	// that pass a test (provided as a function).
+
+
+
+
+
+
+// Declare a game over function 
+	// Goal: Animte the winning chips! (declare function for animation)
+
+
+
+// Declare function that will display Winner!
+	// How to pull board off of screen? (timer?)
+	// How to drop off the loser's chip? (timer?)
+	// Change background color once game ends to 
+	// indicate end of game
+
+// function to end game? //
+// function pullOutIntro() {
+// 	introMsg.style.left = '65px';
+// 	introMsg.style.animationName = 'fadeAway' (animationName not defined)
+// }
